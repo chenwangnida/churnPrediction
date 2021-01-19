1a2d896f4da7ec0911b29762bb9063815e57976a
Chen Wang <chenwang@Chens-MacBook-Pro.local>
2021-01-20 09:32:34 +1300

    final version

--- main.py
+++ main.py
@@ -191,9 +191,10 @@ precision, recall, fscore, _ = score(y_test_prepared, y_pred_lr, average='weight
 accuracy = accuracy_score(y_test_prepared, y_pred_lr)
 
 metrics.append(pd.Series({'precision': precision, 'recall': recall,
-                          'fscore': fscore, 'accuracy': accuracy},
+                          'fscore': fscore, 'accuracy': accuracy, 'roc': roc_auc_score(y_test_prepared, y_pred_lr)},
                          name='lr'))
 
+print(metrics)
 lr_cm = confusion_matrix(y_test_prepared, y_pred_lr)
 sns.heatmap(lr_cm, annot=True, fmt='d', cmap=mpl.cm.binary)
 plt.show()
@@ -221,8 +222,10 @@ precision_svc, recall_svc, fscore_svc, _ = score(y_test_prepared, y_pred_svc, av
 accuracy_svc = accuracy_score(y_test_prepared, y_pred_svc)
 
 metrics.append(pd.Series({'precision': precision_svc, 'recall': recall_svc,
-                          'fscore': fscore_svc, 'accuracy': accuracy_svc},
+                          'fscore': fscore_svc, 'accuracy': accuracy_svc,
+                          'roc': roc_auc_score(y_test_prepared, y_pred_svc)},
                          name='SVC'))
+print(metrics)
 
 cm_svc = confusion_matrix(y_test_prepared, y_pred_svc)
 sns.heatmap(cm_svc, annot=True, fmt='d', cmap=mpl.cm.binary)
@@ -252,8 +255,9 @@ precision_knn, recall_knn, fscore_knn, _knn = score(y_test_prepared, y_pred_knn,
 accuracy_knn = accuracy_score(y_test_prepared, y_pred_knn)
 
 metrics.append(pd.Series({'precision': precision_knn, 'recall': recall_knn,
-                          'fscore': fscore_knn, 'accuracy': accuracy_knn},
+                          'fscore': fscore_knn, 'accuracy': accuracy_knn,  'roc': roc_auc_score(y_test_prepared, y_pred_knn)},
                          name='knn'))
+print(metrics)
 
 knn_cm = confusion_matrix(y_test_prepared, y_pred_knn)
 sns.heatmap(knn_cm, annot=True, fmt='d', cmap=mpl.cm.binary)
@@ -285,9 +289,9 @@ precision_tree, recall_tree, fscore_tree, _tree = score(y_test_prepared, y_pred_
 accuracy_tree = accuracy_score(y_test_prepared, y_pred_tree)
 
 metrics.append(pd.Series({'precision': precision_tree, 'recall': recall_tree,
-                          'fscore': fscore_tree, 'accuracy': accuracy_tree},
+                          'fscore': fscore_tree, 'accuracy': accuracy_tree, 'roc': roc_auc_score(y_test_prepared, y_pred_tree)},
                          name='DTree'))
-
+print(metrics)
 cm_tree = confusion_matrix(y_test_prepared, y_pred_tree)
 sns.heatmap(cm_tree, annot=True, fmt='d', cmap=mpl.cm.binary)
 plt.show()
@@ -317,9 +321,9 @@ precision_rf, recall_rf, fscore_rf, _rf = score(y_test_prepared, y_pred_rf, aver
 accuracy_rf = accuracy_score(y_test_prepared, y_pred_rf)
 
 metrics.append(pd.Series({'precision': precision_rf, 'recall': recall_rf,
-                          'fscore': fscore_rf, 'accuracy': accuracy_rf},
+                          'fscore': fscore_rf, 'accuracy': accuracy_rf, 'roc': roc_auc_score(y_test_prepared, y_pred_rf)},
                          name='RandomForest'))
-
+print(metrics)
 cm_rf = confusion_matrix(y_test_prepared, y_pred_rf)
 sns.heatmap(cm_rf, annot=True, fmt='d', cmap=mpl.cm.binary)
 plt.show()
@@ -352,9 +356,9 @@ precision_extrTree, recall_extrTree, fscore_extrTree, _extrTree = score(y_test_p
 accuracy_extrTree = accuracy_score(y_test_prepared, y_pred_extrTree)
 
 metrics.append(pd.Series({'precision': precision_extrTree, 'recall': recall_extrTree,
-                          'fscore': fscore_extrTree, 'accuracy': accuracy_extrTree},
+                          'fscore': fscore_extrTree, 'accuracy': accuracy_extrTree, 'roc': roc_auc_score(y_test_prepared, y_pred_extrTree)},
                          name='ExtraTrees'))
-
+print(metrics)
 cm_extrTree = confusion_matrix(y_test_prepared, y_pred_extrTree)
 sns.heatmap(cm_extrTree, annot=True, fmt='d', cmap=mpl.cm.binary)
 plt.show()
@@ -389,9 +393,9 @@ precision_gboost, recall_gboost, fscore_gboost, _gboost = score(y_test_prepared,
 accuracy_gboost = accuracy_score(y_test_prepared, y_pred_gboost)
 
 metrics.append(pd.Series({'precision': precision_gboost, 'recall': recall_gboost,
-                          'fscore': fscore_gboost, 'accuracy': accuracy_gboost},
+                          'fscore': fscore_gboost, 'accuracy': accuracy_gboost, 'roc': roc_auc_score(y_test_prepared, y_pred_gboost)},
                          name='GradientBoosting'))
-
+print(metrics)
 cm_gboost = confusion_matrix(y_test_prepared, y_pred_gboost)
 sns.heatmap(cm_gboost, annot=True, fmt='d', cmap=mpl.cm.binary)
 plt.show()
@@ -421,8 +425,9 @@ precision_aboost, recall_aboost, fscore_aboost, _aboost = score(y_test_prepared,
 accuracy_aboost = accuracy_score(y_test_prepared, y_pred_aboost)
 
 metrics.append(pd.Series({'precision': precision_aboost, 'recall': recall_aboost,
-                          'fscore': fscore_aboost, 'accuracy': accuracy_aboost},
+                          'fscore': fscore_aboost, 'accuracy': accuracy_aboost, 'roc': roc_auc_score(y_test_prepared, y_pred_aboost)},
                          name='AdamBoosting'))
+print(metrics)
 
 cm_aboost = confusion_matrix(y_test_prepared, y_pred_aboost)
 sns.heatmap(cm_aboost, annot=True, fmt='d', cmap=mpl.cm.binary)
@@ -463,19 +468,18 @@ ax.set_title('Accuracy over iterations')
 plt.show()
 # %%
 model_1_read = keras.models.load_model('knn1')
-#print(run_hist_1.history.keys())
+# print(run_hist_1.history.keys())
 y_pred_knn1 = model_1_read.predict_classes(X_test_prepared)
 precision_nn1, recall_nn1, fscore_nn1, _ = score(y_test_prepared, y_pred_knn1, average='weighted')
 accuracy_nn1 = accuracy_score(y_test_prepared, y_pred_knn1)
+roc_nn1 = roc_auc_score(y_test_prepared, y_pred_knn1)
 
-print(precision_nn1, recall_nn1, fscore_nn1, accuracy_nn1)
-
-
+print(precision_nn1, recall_nn1, fscore_nn1, accuracy_nn1, roc_nn1)
 
 # %%
 model_2 = Sequential()
 model_2.add(Dense(50, input_shape=(73,), activation="relu"))
-model_2.add(Dense(50,  activation="relu"))
+model_2.add(Dense(50, activation="relu"))
 model_2.add(Dense(1, activation="sigmoid"))
 
 model_2.summary()
@@ -501,126 +505,15 @@ ax.plot(range(n), (run_hist_2.history["val_accuracy"]), 'b.', label="Validation 
 ax.legend(loc='lower right')
 ax.set_title('Accuracy over iterations')
 plt.show()
+print(run_hist_2.history.keys())
+
 # %%
 model_2_read = keras.models.load_model('knn2')
-print(run_hist_2.history.keys())
+#%%
 y_pred_knn2 = model_2_read.predict_classes(X_test_prepared)
 precision_nn2, recall_nn2, fscore_nn2, _ = score(y_test_prepared, y_pred_knn2, average='weighted')
 accuracy_nn2 = accuracy_score(y_test_prepared, y_pred_knn2)
+roc_nn2 = roc_auc_score(y_test_prepared, y_pred_knn2)
 
-print(precision_nn2, recall_nn2, fscore_nn2, accuracy_nn2)
-# %% Plotting the Kaplan-Meier Curve
-from lifelines import KaplanMeierFitter, CoxPHFitter
+print(precision_nn2, recall_nn2, fscore_nn2, accuracy_nn2, roc_nn2)
 
-kmf = KaplanMeierFitter()
-
-kmf.fit(data.account_length, data.churn, label='Kaplan Meier Estimate, full sample')
-
-kmf.plot(linewidth=4, figsize=(12, 6))
-plt.title('Customer Churn: Kaplan-Meier Curve')
-plt.xlabel('Months')
-plt.ylabel('Survival probability')
-plt.show()
-
-# %% Plotting the Kaplan-Meier Curve for two different groups
-
-df1 = train[train.international_plan == 0]
-df2 = train[train.international_plan == 1]
-
-kmf.fit(df1.account_length, df1.churn)
-kmf.plot(label='Domestic Plan', figsize=(12, 6))
-kmf.fit(df2.account_length, df2.churn)
-kmf.plot(label='International Plan')
-plt.title('International Plan and Churn: Kaplan-Meier Curve')
-plt.xlabel('Months')
-plt.ylabel('Survival probability')
-plt.show()
-
-df1 = train[train.voice_mail_plan == 0]
-df2 = train[train.voice_mail_plan == 1]
-
-kmf.fit(df1.account_length, df1.churn)
-kmf.plot(label='Voice Mail Plan', figsize=(12, 6))
-kmf.fit(df2.account_length, df2.churn)
-kmf.plot(label='Voice Mail Plan')
-plt.title('Voice Mail Plan and Churn: Kaplan-Meier Curve')
-plt.xlabel('Months')
-plt.ylabel('Survival probability')
-plt.show()
-# %%
-from lifelines.utils.sklearn_adapter import sklearn_adapter
-
-from lifelines import CoxPHFitter
-from sklearn.model_selection import cross_val_score
-
-X = train.copy().drop('account_length', axis=1).copy()  # keep as a dataframe
-Y = train.copy().pop('account_length').copy()
-
-base_cox = sklearn_adapter(CoxPHFitter, event_col='churn')
-wf = base_cox()
-
-scores = cross_val_score(wf, X, Y, cv=10)
-print(np.mean(scores))
-
-# %% Fitting Cox Proportional Model
-
-from lifelines import WeibullAFTFitter
-
-X = train.copy().drop('account_length', axis=1)  # keep as a dataframe
-Y = train.copy().pop('account_length').copy()
-base_aft = sklearn_adapter(WeibullAFTFitter, event_col='churn')
-aft = base_aft()
-
-scores = cross_val_score(aft, X, Y, cv=10)
-print(np.mean(scores))
-
-# %%
-from lifelines import LogLogisticAFTFitter
-from lifelines import LogNormalAFTFitter
-
-X = train.copy().drop('account_length', axis=1)  # keep as a dataframe
-Y = train.copy().pop('account_length').copy()
-base_lognor = sklearn_adapter(LogNormalAFTFitter, event_col='churn')
-lognor = base_lognor()
-
-scores = cross_val_score(lognor, X, Y, cv=10)
-print(np.mean(scores))
-
-# %%
-from sklearn.model_selection import GridSearchCV
-
-clf = GridSearchCV(wf, {
-    "penalizer": 10.0 ** np.arange(-2, 3),
-    "l1_ratio": [0, 1 / 3, 2 / 3],
-    "model_ancillary": [True, False],
-}, cv=4)
-clf.fit(X, Y)
-
-print(clf.best_estimator_)
-
-from sklearn.model_selection import train_test_split
-
-# Training and Validation set split based on the stratified churn groups
-X = train.drop('churn', axis=1).copy()
-
-y = train['churn'].copy()
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, stratify=y)
-
-# %% test boosting works or not
-import xgboost as xgb
-
-clf_xgb = xgb.XGBClassifier(seed=42,
-                            objective='binary:logistic',
-                            gamma=0.25,
-                            learn_rate=0.01,
-                            max_depth=4,
-                            reg_lambda=10,
-                            scale_pos_weight=1,
-                            subsample=0.9,
-                            colsample_bytree=0.5)
-clf_xgb.fit(X_train,
-            y_train,
-            verbose=True,
-            early_stopping_rounds=10,
-            eval_metric='aucpr',
-            eval_set=[(X_test, y_test)])
